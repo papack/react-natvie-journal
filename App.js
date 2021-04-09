b0380c90b3c98dde87813e1da5cf7022f43209ff
Matthias Steiner <m@kjpjm.de>
2021-04-09 22:35:33 +0200

    add entry is now possible

--- App.js
+++ App.js
@@ -1,18 +1,32 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
-import { StyleSheet, TextInput, Text, View } from "react-native";
+import { StyleSheet, FlatList, TextInput, Text, View } from "react-native";
 
 export default function App() {
   const [text, setText] = useState("");
+  const [data, setData] = useState([]);
+
+  const handleSubmitEdit = () => {
+    setData([...data, { date: new Date().toTimeString(), text: text }]);
+    setText("");
+  };
 
   return (
     <View style={styles.container}>
-      <Text>{text || "No entry in the diary.."}</Text>
+      <FlatList
+        data={data}
+        keyExtractor={(item) => item.date}
+        renderItem={({ item }) => <Text>{item.text}</Text>}
+      />
       <TextInput
         returnKeyType="done"
         style={styles.input}
         placeholder="Tagebucheintrag..."
-        onChangeText={(text) => setText(text)}
+        value={text}
+        onChangeText={(text) => {
+          setText(text.toString());
+        }}
+        onSubmitEditing={handleSubmitEdit}
       />
       <StatusBar style="auto" />
     </View>
